1c28edbd3ccbeead3984fc32d01527d2c138d5de
teju-26 <110175698+teju-26@users.noreply.github.com>
2024-02-08 11:22:25 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+//num
+let height= 56;
+let width=89;
+typeof 'height'; 
+console.log(typeof height);//object
+
+
+//stirng
+let name="teju"
+typeof 'name';
+console.log(typeof name)//string
+
+
+//Boolenas
+let x= true;
+let y=false;
+console.log(typeof x);
+
+
+//object
+
+const person ={firstname:"teju",lastname:"borekar"};
+console.log(typeof person);
