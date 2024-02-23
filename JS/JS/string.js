cb875466cf9d4324c9efcd3caa47b785f560be17
teju-26 <110175698+teju-26@users.noreply.github.com>
2024-02-23 21:07:56 +0530

    Add files via upload

---
+++ JS/JS/string.js
@@ -0,0 +1,27 @@
+/*var string ="teju"
+let length = string.length
+console.log(length)
+
+
+let index=string.charAt(2)
+console.log(index)
+
+
+let letter = string.at(4)
+console.log(letter)
+
+var str= 'apple,mango , chikku';
+let part=str.slice(0,4);
+console.log(part)
+
+
+var number='123';
+var num =123;
+console.log(typeof(number))
+console.log(typeof(Number(number)))
+*/
+
+
+let sing ="bshydjwtdjyd";
+let text = `my name is ${sing}`;
+console.log(text)
