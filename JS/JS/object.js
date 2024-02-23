0440f75da5c508399241fec7eabee71122127a08
teju-26 <110175698+teju-26@users.noreply.github.com>
2024-02-23 21:08:41 +0530

    Add files via upload

---
+++ JS/JS/object.js
@@ -0,0 +1,119 @@
+
+/*var student={
+    name:"teju",
+    age:21,
+
+    skill:['Cloud','Design']
+}
+console.log(student);
+console.log(student.name);
+
+
+var student2 = student
+student2.name ="VCET"
+//console.log(student.name)
+console.log(student2  == student)
+console.log(student2  === student)
+
+/*var str='vcet it'
+var result = str.toUpperCase();
+console.log(result)
+console.log(str)
+
+
+
+function blockscope(){
+    let  a=5;
+   console.log(a)
+}
+
+let a=7;
+console.log(a)
+console.log(blockscope())*/
+
+
+/*function scope(){
+    var b=8
+    console.log(b)
+}
+var b=7
+console.log(b)//7
+console.log(scope())//8
+
+
+/*var stu ={
+    name1:'vcet',
+    loction:'vasai',
+
+}
+*/
+
+
+
+
+/*var time ={
+    //name2:'ohjj',
+    name:funtion(){
+        var c=7;
+       console.log(c)
+       console.log(name2)
+        console.log (funtion())
+    }
+    }*/
+
+
+    /*function add(a,b){
+      var c= a+b;
+      return c;
+    }
+    add(8,9)
+    console.log(add())
+    console.log(add(5,7))*/
+
+
+    /*var traingle = new Object();
+    traingle.a = 3;
+    traingle.b = 8;
+    traingle. getArea =  function(a,b){
+     var area =(a+b)
+   // console.log(area)
+    //console.log(getArea())
+
+    return (area); 
+    };
+*/
+
+
+ /*const stu_info = {
+    Namme:'teju',
+    barch:'IT',
+    skills:['gch','fdgh'],
+}
+console.log(stu_info);
+stu_info.age=45;//adding new property into const object/ as using const it get changed because obj mutable 
+console.log(stu_info);
+stu_info={
+skiils:['hjk','hji'],
+}
+console.log(stu)*/
+
+
+
+let person={
+    name:'teju',
+    age:21,
+    address:{
+        city:'virar',
+        bulidngl:'gokul park',
+        pin:401303
+    },
+    date:{
+      dateee:17/2/2024,
+        day:'sat',
+        month:'feb'
+
+    }
+};
+console.log(person.name)
+console.log(person.address.pin)
+console.log(person.date.day)
